1d52d105c6d7740f9a5fd52cc50cf7d4569a2c3b
Clumsy <62894525+clumsyclems@users.noreply.github.com>
2021-05-05 15:05:26 +0200

    Update viewFlight.js

--- New_Front/viewFlight.js
+++ New_Front/viewFlight.js
@@ -26,7 +26,7 @@ function userCalldone(result){
     //for the img
 	tmp.innerHTML = userDisplay;
     let Display = document.getElementById("Pilot_Info");
-    Display.innerHTML = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.innerHTML = tmp.textContent.replace(/[ "']+/g, "");
 
 }
 
@@ -101,52 +101,52 @@ function callDone(result){
     //for the img
 	tmp.innerHTML = img;
     let Display = document.getElementById("Flight-Picture");
-    Display.src = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.src = tmp.textContent.replace(/[ "']+/g, "");
 
     //for the title
 	tmp.innerHTML = title;
     Display = document.getElementById("title");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     //for the type of flight
     tmp.innerHTML = typeOfFlight;
     Display = document.getElementById("typeOfFlight");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     // for the number of seat
     tmp.innerHTML = seats;
     Display = document.getElementById("numberOfSeat");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     // for the duration
     tmp.innerHTML = duration;
     Display = document.getElementById("Duration");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     // for the Departure Date
     tmp.innerHTML = departureDate;
     Display = document.getElementById("Departure_Date");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     // for the Departure Place
     tmp.innerHTML = departure;
     Display = document.getElementById("Departure_Place");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     // for the Arrival Place
     tmp.innerHTML = arrival;
     Display = document.getElementById("Arrival_Place");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     // for the Description
     tmp.innerHTML = flightDescription;
     Display = document.getElementById("Description");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     // for the Aircraft Information
     tmp.innerHTML = aircraft;
     Display = document.getElementById("Aircraft_Info");
-    Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+    Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
     // for the Pilot Information
     tmp.innerHTML = pilot;
@@ -155,7 +155,7 @@ function callDone(result){
 	
 	tmp.innerHTML = price;
 	Display = document.getElementById("price");
-	Display.textContent = tmp.textContent.replace(/[^a-zA-Z0-9.\/]/g, "");
+	Display.textContent = tmp.textContent.replace(/[ "']+/g, "");
 
 }
 
